bd31ef8517279d58e925cb7566bd6281dd555652
saikrishnaoduru841 <89731382+saikrishnaoduru841@users.noreply.github.com>
2022-12-01 13:35:26 +0530

    Update countparameter.tf

--- countparameter.tf
+++ countparameter.tf
@@ -8,9 +8,24 @@ resource "aws_instance" "Ec2-1" {
     count = 5
 }
 
-
+----------------------------
 resource "aws_iam_user" "iam" {
     name = "sai".${count.index}
     count = 5
+    path = "/system/"
+}
+-----------------------------
+    
+provider "aws" {
+    region = "us-east-1"
+}
+
+variable "elb_names" {
+    type = list
+    default = ["dev-sai","stage-sai","prod-sai"]
+
+resource "aws_iam_user" "iam" {
+    name = var.elb_names[count.index]
+    count = 3
     path = "/system/"
 }
